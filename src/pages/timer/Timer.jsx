d2037a9c6e43dbba0178aeb014fe27b7ebc77d7e
Evgeny Vinnik <evgenyvinnik@gmail.com>
2025-11-03 09:53:42 -0800

    Implemented countdown timer

--- src/pages/timer/Timer.jsx
+++ src/pages/timer/Timer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import "./styles.css";
 
@@ -6,29 +6,21 @@ const SECOND = 1000;
 
 export default function Timer() {
   const [running, setRunning] = useState(false);
+
   const [hour, setHour] = useState(0);
   const [minute, setMinute] = useState(0);
   const [second, setSecond] = useState(0);
-  const [total, setTotal] = useState(0);
-  const [id, setId] = useState(null);
 
-  const [reset, setReset] = useState({ hour: 0, minute: 0, second: 0 });
+  const [total, setTotal] = useState(0);
+
+  const [id, setId] = useState(null);
 
   const calculateTotal = (hour, minute, second) =>
     Number(hour * 3600) + Number(minute * 60) + Number(second);
 
   useEffect(() => {
-    if (running) {
-      setId(
-        setInterval(() => {
-          setTotal((prev) => prev - 1);
-        }, SECOND)
-      );
-    } else {
-      clearInterval(id);
-      setId(null);
-    }
-  }, [running]);
+    setTotal(calculateTotal(hour, minute, second));
+  }, [hour, minute, second]);
 
   useEffect(() => {
     if (running && total === 0) {
@@ -40,26 +32,39 @@ export default function Timer() {
   }, [total, running]);
 
   const onClickStart = () => {
-    const counts = calculateTotal(hour, minute, second);
-    console.log("calculateTotal", counts);
-    setTotal(counts);
+    clearInterval(id);
+
+    setId(
+      setInterval(() => {
+        setTotal((prev) => prev - 1);
+      }, SECOND)
+    );
+
     setRunning(true);
-    setReset({
-      hour,
-      minute,
-      second,
-    });
   };
 
   const onClickPause = () => {
-    setRunning(false);
+    clearInterval(id);
+    setId(null);
   };
-  const onClickReset = () => {
-    setHour(reset.hour);
-    setMinute(reset.minute);
-    setSecond(reset.second);
 
-    setTotal(calculateTotal(reset.hour, reset.minute, reset.second))
+  const onClickReset = () => {
+    setRunning(false);
+    clearInterval(id);
+    setId(null);
+    setTotal(calculateTotal(hour, minute, second));
+  };
+
+  const timeString = (total) => {
+    const hour = Math.floor(total / 3600);
+    let frac = total - hour * 3600;
+    const min = Math.floor(frac / 60);
+    const sec = frac - min * 60;
+
+    return `${String(hour).padStart(2, "0")}:${String(min).padStart(
+      2,
+      "0"
+    )}:${String(sec).padStart(2, "0")}`;
   };
 
   return (
@@ -67,38 +72,54 @@ export default function Timer() {
       <h1 className="header">Countdown timer</h1>
       <div className="wrapper">
         {running ? (
-          <div className="running">
-            {total}
-            {/* {hour} : {minute} : {second} */}
-          </div>
+          <div className="running">{timeString(total)}</div>
         ) : (
           <div className="inputs">
             <input
               className="timeInput"
+              type="number"
               value={hour}
+              max={60}
+              min={0}
+              onBlur={() => {
+                if (hour < 0) setHour(0);
+                else if (hour > 60) setHour(60);
+              }}
               onChange={(e) => setHour(e.target.value)}
             />
             <input
+              type="number"
               className="timeInput"
+              max={60}
+              min={0}
               value={minute}
+              onBlur={() => {
+                if (hour < 0) setMinute(0);
+                else if (hour > 60) setMinute(60);
+              }}
               onChange={(e) => setMinute(e.target.value)}
             />
             <input
               className="timeInput"
+              type="number"
+              max={60}
+              min={0}
               value={second}
+              onBlur={() => {
+                if (hour < 0) setSecond(0);
+                else if (hour > 60) setSecond(60);
+              }}
               onChange={(e) => setSecond(e.target.value)}
             />
           </div>
         )}
         <div className="buttons">
-          {running ? (
-            <>
-              <button onClick={onClickPause}>Pause</button>
-              <button onClick={onClickReset}>Reset</button>
-            </>
-          ) : (
+          {id == null ? (
             <button onClick={onClickStart}>Start</button>
+          ) : (
+            <button onClick={onClickPause}>Pause</button>
           )}
+          {running && <button onClick={onClickReset}>Reset</button>}
         </div>
       </div>
     </main>
